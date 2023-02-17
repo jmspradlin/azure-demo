4f5b19fc3b65584e917bdb23fe6f8cefe7340165
Jeff Spradlin <jeff.spradlin@slalom.com>
2023-02-17 16:19:11 -0600

    adding variable declarations

--- variables.tf
+++ variables.tf
@@ -11,10 +11,10 @@ variable "aws_public_rules" {}
 variable "aws_private_rules" {}
 
 # Azure Resources
-# variable "vnet" {}
-# variable "subnets" {}
-# variable "azure_load_balancer" {}
-# variable "azure_linux_vms" {}
-# variable "nsg" {}
-# variable "nsg_rules_public" {}
-# variable "nsg_rules_private" {}
+variable "vnet" {}
+variable "subnets" {}
+variable "azure_load_balancer" {}
+variable "azure_linux_vms" {}
+variable "nsg" {}
+variable "nsg_rules_public" {}
+variable "nsg_rules_private" {}
