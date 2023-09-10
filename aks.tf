21c7fda1ba8e7a6cd42ba8abea36fff106c88dad
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-09-09 20:13:51 -0500

    updating k8s to be simpler

---
+++ aks.tf
@@ -0,0 +1,17 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.env}-${var.rg.name}-01"
+  resource_group_name = azurerm_resource_group.rg01.name
+  location            = azurerm_resource_group.rg01.location
+  dns_prefix          = "${var.env}-${var.rg.name}-01"
+  default_node_pool {
+    name           = "default"
+    node_count     = 1
+    vm_size        = "Standard_B2s"
+    vnet_subnet_id = azurerm_subnet.subnets["default_node_pool"].id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
