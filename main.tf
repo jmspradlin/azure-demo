b39287fe306c854ad8470fc784f04525c2866ffc
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-09-12 10:32:47 -0500

    flipping variables

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "rg" {
   source  = "app.terraform.io/jeff-spradlin-org/rg/azurerm"
   version = "1.1.0"
   # insert required variables here
-  env = var.env
-  rg_name = var.rg_name
+  env = var.rg_name
+  rg_name = var.env
   rg_location = var.rg_location
 }
 
